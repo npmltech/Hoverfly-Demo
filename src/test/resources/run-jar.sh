09c475ce43f60bd64d71c2ce3cb22824a6311070
npmltech <devqasp@gmail.com>
2023-11-22 18:25:41 -0300

    new jar has been created

--- src/test/resources/run-jar.sh
+++ src/test/resources/run-jar.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 # shellcheck disable=SC2164
+# path of your project
 cd ~/Projects/Java/Hoverfly-Demo/src/test/resources/
-java -jar hoverfly-java-middleware.jar
+# middleware execution
+java -jar hoverfly-java-middleware-1.jar
